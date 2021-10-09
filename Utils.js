2b123da22ec76e695fcb0ced7a46265877e5a34a
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2021-10-09 14:57:46 +0800

    初始化仓库

---
+++ Utils.js
@@ -0,0 +1,336 @@
+// ==UserScript==
+// @name         Utils环境
+// @namespace    雪导.
+// @version      1.0
+// @description  一个javascript自用环境，封装了常用工具类
+// @author       雪导
+// @grant        none
+// ==/UserScript==
+function Utils(name, opts) {
+  return new (class {
+    constructor(name, opts) {
+      this.name = name
+      this.data = null
+      this.dataFile = 'box.dat'
+      this.logs = []
+      this.logSeparator = '\n'
+      this.startTime = new Date().getTime()
+      Object.assign(this, opts)
+      this.log('', `🔔${this.name}, 开始!`)
+    }
+
+    toObj(str, defaultValue = null) {
+      try {
+        return JSON.parse(str)
+      } catch {
+        return defaultValue
+      }
+    }
+
+    toStr(obj, defaultValue = null) {
+      try {
+        return JSON.stringify(obj)
+      } catch {
+        return defaultValue
+      }
+    }
+
+    getjson(key, defaultValue) {
+      let json = defaultValue
+      const val = this.getdata(key)
+      if (val) {
+        try {
+          json = JSON.parse(this.getdata(key))
+        } catch {}
+      }
+      return json
+    }
+
+    setjson(val, key) {
+      try {
+        return this.setdata(JSON.stringify(val), key)
+      } catch {
+        return false
+      }
+    }
+
+    getScript(url) {
+      return new Promise(resolve => {
+        this.get({ url }, (err, resp, body) => resolve(body))
+      })
+    }
+
+    loaddata() {
+      if (this.isNode()) {
+        this.fs = this.fs ? this.fs : require('fs')
+        this.path = this.path ? this.path : require('path')
+        const curDirDataFilePath = this.path.resolve(this.dataFile)
+        const rootDirDataFilePath = this.path.resolve(
+          process.cwd(),
+          this.dataFile
+        )
+        const isCurDirDataFile = this.fs.existsSync(curDirDataFilePath)
+        const isRootDirDataFile =
+          !isCurDirDataFile && this.fs.existsSync(rootDirDataFilePath)
+        if (isCurDirDataFile || isRootDirDataFile) {
+          const datPath = isCurDirDataFile
+            ? curDirDataFilePath
+            : rootDirDataFilePath
+          try {
+            return JSON.parse(this.fs.readFileSync(datPath))
+          } catch (e) {
+            return {}
+          }
+        } else return {}
+      } else return {}
+    }
+
+    writedata() {
+      if (this.isNode()) {
+        this.fs = this.fs ? this.fs : require('fs')
+        this.path = this.path ? this.path : require('path')
+        const curDirDataFilePath = this.path.resolve(this.dataFile)
+        const rootDirDataFilePath = this.path.resolve(
+          process.cwd(),
+          this.dataFile
+        )
+        const isCurDirDataFile = this.fs.existsSync(curDirDataFilePath)
+        const isRootDirDataFile =
+          !isCurDirDataFile && this.fs.existsSync(rootDirDataFilePath)
+        const jsondata = JSON.stringify(this.data)
+        if (isCurDirDataFile) {
+          this.fs.writeFileSync(curDirDataFilePath, jsondata)
+        } else if (isRootDirDataFile) {
+          this.fs.writeFileSync(rootDirDataFilePath, jsondata)
+        } else {
+          this.fs.writeFileSync(curDirDataFilePath, jsondata)
+        }
+      }
+    }
+
+    lodash_get(source, path, defaultValue = undefined) {
+      const paths = path.replace(/\[(\d+)\]/g, '.$1').split('.')
+      let result = source
+      for (const p of paths) {
+        result = Object(result)[p]
+        if (result === undefined) {
+          return defaultValue
+        }
+      }
+      return result
+    }
+
+    lodash_set(obj, path, value) {
+      if (Object(obj) !== obj) return obj
+      if (!Array.isArray(path)) path = path.toString().match(/[^.[\]]+/g) || []
+      path
+        .slice(0, -1)
+        .reduce(
+          (a, c, i) =>
+            Object(a[c]) === a[c]
+              ? a[c]
+              : (a[c] = Math.abs(path[i + 1]) >> 0 === +path[i + 1] ? [] : {}),
+          obj
+        )[path[path.length - 1]] = value
+      return obj
+    }
+
+    getdata(key) {
+      let val = this.getval(key)
+      // 如果以 @
+      if (/^@/.test(key)) {
+        const [, objkey, paths] = /^@(.*?)\.(.*?)$/.exec(key)
+        const objval = objkey ? this.getval(objkey) : ''
+        if (objval) {
+          try {
+            const objedval = JSON.parse(objval)
+            val = objedval ? this.lodash_get(objedval, paths, '') : val
+          } catch (e) {
+            val = ''
+          }
+        }
+      }
+      return val
+    }
+
+    setdata(val, key) {
+      let issuc = false
+      if (/^@/.test(key)) {
+        const [, objkey, paths] = /^@(.*?)\.(.*?)$/.exec(key)
+        const objdat = this.getval(objkey)
+        const objval = objkey
+          ? objdat === 'null'
+            ? null
+            : objdat || '{}'
+          : '{}'
+        try {
+          const objedval = JSON.parse(objval)
+          this.lodash_set(objedval, paths, val)
+          issuc = this.setval(JSON.stringify(objedval), objkey)
+        } catch (e) {
+          const objedval = {}
+          this.lodash_set(objedval, paths, val)
+          issuc = this.setval(JSON.stringify(objedval), objkey)
+        }
+      } else {
+        issuc = this.setval(val, key)
+      }
+      return issuc
+    }
+
+    getval(key) {
+      if (this.isSurge() || this.isLoon()) {
+        return $persistentStore.read(key)
+      } else if (this.isQuanX()) {
+        return $prefs.valueForKey(key)
+      } else if (this.isNode()) {
+        this.data = this.loaddata()
+        return this.data[key]
+      } else {
+        return (this.data && this.data[key]) || null
+      }
+    }
+
+    setval(val, key) {
+      if (this.isSurge() || this.isLoon()) {
+        return $persistentStore.write(val, key)
+      } else if (this.isQuanX()) {
+        return $prefs.setValueForKey(val, key)
+      } else if (this.isNode()) {
+        this.data = this.loaddata()
+        this.data[key] = val
+        this.writedata()
+        return true
+      } else {
+        return (this.data && this.data[key]) || null
+      }
+    }
+
+    /**
+     *  时间格式化
+     * 示例:$.time('yyyy-MM-dd qq HH:mm:ss.S')
+     *    :$.time('yyyyMMddHHmmssS')
+     *    y:年 M:月 d:日 q:季 H:时 m:分 s:秒 S:毫秒
+     *    其中y可选0-4位占位符、S可选0-1位占位符，其余可选0-2位占位符
+     * @param {string} fmt 格式化参数
+     * @param {number} 可选: 根据指定时间戳返回格式化日期
+     *
+     */
+    formatTime(fmt, ts = null) {
+      const date = ts ? new Date(ts) : new Date()
+      let o = {
+        'M+': date.getMonth() + 1,
+        'd+': date.getDate(),
+        'H+': date.getHours(),
+        'm+': date.getMinutes(),
+        's+': date.getSeconds(),
+        'q+': Math.floor((date.getMonth() + 3) / 3),
+        S: date.getMilliseconds(),
+      }
+      if (/(y+)/.test(fmt))
+        fmt = fmt.replace(
+          RegExp.$1,
+          (date.getFullYear() + '').substr(4 - RegExp.$1.length)
+        )
+      for (let k in o)
+        if (new RegExp('(' + k + ')').test(fmt))
+          fmt = fmt.replace(
+            RegExp.$1,
+            RegExp.$1.length == 1
+              ? o[k]
+              : ('00' + o[k]).substr(('' + o[k]).length)
+          )
+      return fmt
+    }
+
+    /**
+     * 获取数组中的 最大值
+     * @param {Array} arr
+     * @returns {Number}
+     */
+    getMaxNumOfArr = arr => {
+      let num = arr[0]
+      for (let i = 0; i < arr.length; i++) {
+        if (num < arr[i]) {
+          num = arr[i]
+        }
+      }
+      return num
+    }
+
+    /**
+     * 判断两个字符串 相似度
+     * @param {String} s 第一个字符串
+     * @param {String} t 第二个字符串
+     * @returns {Number} 相似度
+     */
+    strSimilarity2Percent = (s, t) => {
+      const strSimilarity2Number = (s, t) => {
+        let n = s.length,
+          m = t.length,
+          d = []
+        let i, j, s_i, t_j, cost
+        if (n == 0) return m
+        if (m == 0) return n
+        for (i = 0; i <= n; i++) {
+          d[i] = []
+          d[i][0] = i
+        }
+        for (j = 0; j <= m; j++) {
+          d[0][j] = j
+        }
+        for (i = 1; i <= n; i++) {
+          s_i = s.charAt(i - 1)
+          for (j = 1; j <= m; j++) {
+            t_j = t.charAt(j - 1)
+            if (s_i == t_j) {
+              cost = 0
+            } else {
+              cost = 1
+            }
+            d[i][j] = Minimum(
+              d[i - 1][j] + 1,
+              d[i][j - 1] + 1,
+              d[i - 1][j - 1] + cost
+            )
+          }
+        }
+        return d[n][m]
+      }
+      const Minimum = (a, b, c) => {
+        return a < b ? (a < c ? a : c) : b < c ? b : c
+      }
+
+      let l = s.length > t.length ? s.length : t.length
+      let d = strSimilarity2Number(s, t)
+      return (1 - d / l).toFixed(4)
+    }
+
+    log(...logs) {
+      if (logs.length > 0) {
+        this.logs = [...this.logs, ...logs]
+      }
+      console.log(logs.join(this.logSeparator))
+    }
+
+    logErr(err, msg) {
+      const isPrintSack = !this.isSurge() && !this.isQuanX() && !this.isLoon()
+      if (!isPrintSack) {
+        this.log('', `❗️${this.name}, 错误!`, err)
+      } else {
+        this.log('', `❗️${this.name}, 错误!`, err.stack)
+      }
+    }
+
+    wait(time) {
+      return new Promise(resolve => setTimeout(resolve, time))
+    }
+
+    done() {
+      const endTime = new Date().getTime()
+      const costTime = (endTime - this.startTime) / 1000
+      this.log('', `🔔${this.name}, 结束! 🕛 ${costTime} 秒`)
+      this.log()
+    }
+  })(name, opts)
+}
